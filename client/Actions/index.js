b417a47d16d1eabc8f60837cbc216a7f95aa8291
VisH317 <54584201+VisH317@users.noreply.github.com>
2019-08-29 13:12:34 -0400

    Update index.js

--- client/Actions/index.js
+++ client/Actions/index.js
@@ -4,5 +4,5 @@ import keys from './types'
 export const fetchUser = () => async dispatch => {
     const response = await axios.get('/api/current_user')
 
-    dispatch({type: keys.fetchUserKey, payload: response})
+    dispatch({type: keys.fetchUserKey, payload: response.data})
 }
